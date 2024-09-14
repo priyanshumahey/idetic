d64ceb2d190ab6d56e27ee56ba7babb1713d37c8
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2024-09-14 13:18:46 -0700

    User's videos

---
+++ app/convex/listUserVideos.ts
@@ -0,0 +1,21 @@
+import { v } from "convex/values";
+import { query } from "./_generated/server";
+
+export const listUserVideos = query({
+  args: { userId: v.string() },
+  handler: async (ctx, args) => {
+    const videos = await ctx.db
+      .query("videos")
+      .filter((q) => q.eq(q.field("author"), args.userId))
+      .collect();
+
+    return Promise.all(
+      videos.map(async (video) => ({
+        ...video,
+        ...(video.format === "video"
+          ? { url: await ctx.storage.getUrl(video.body) }
+          : {}),
+      }))
+    );
+  },
+});
