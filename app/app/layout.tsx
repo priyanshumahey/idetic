a579625a712a62cd711080600f3c21454dcecde8
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2024-09-14 00:10:52 -0700

    Removed weird font issues

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,20 +1,7 @@
 import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from 'next/font/google';
 import "./globals.css";
-import { Inter } from 'next/font/google'
-
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 const inter = Inter({
   subsets: ['latin'],
