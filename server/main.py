a666ccc7fb72d45afd3e3f9425836456d1a6385b
Vedaant <vedaant.varshney@gmail.com>
2024-09-14 12:36:32 -0400

    Working chunking and transcription generation

--- server/main.py
+++ server/main.py
@@ -4,7 +4,7 @@ import subprocess
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-from utils import transcribe
+from utils.utils import split_audio
 
 import ffmpeg
 from dotenv import load_dotenv
@@ -37,44 +37,13 @@ def process(file_item: FileItem):
 
     output_chunk_dir = f"data/processed/{file_name}/{file_name}_chunks"
 
-    split_audio(audio_file_path, output_chunk_dir, 10)
+    split_audio(audio_file_path, output_chunk_dir, 15)
 
     for file in os.listdir(output_chunk_dir):
-        result = subprocess.run(["./transcribe.sh", f"{output_chunk_dir}/{file}"], capture_output=True, text=True)
+        result = subprocess.run(["./transcribe.sh", output_chunk_dir, file], capture_output=True, text=True)
         print(result)
 
 
 
 
-    # result = subprocess.run(["./transcribe.sh"], capture_output=True, text=True)
-    # print(result)
-
-
-
-
     return {"Response": "Success"}
-
-
-
-def split_audio(input_file, output_dir, chunk_length=10):
-    # Get the filename without the extension and directory
-    filename, ext = os.path.splitext(os.path.basename(input_file))
-    
-    # Create an output directory if it doesn't exist
-    os.makedirs(output_dir, exist_ok=True)
-
-    # Run ffmpeg command to split the audio into chunks
-    ffmpeg_command = [
-        'ffmpeg', 
-        '-i', input_file,               # Input file
-        '-f', 'segment',                # Format as segments
-        '-segment_time', str(chunk_length), # Split by this duration (in seconds)
-        '-c', 'copy',                   # Copy codec to avoid re-encoding
-        os.path.join(output_dir, f"{filename}_%03d{ext}") # Output file format
-    ]
-    
-    try:
-        subprocess.run(ffmpeg_command, check=True)
-        print(f"Audio file successfully split into chunks in '{output_dir}'")
-    except subprocess.CalledProcessError as e:
-        print(f"Error splitting audio file: {e}")
--- server/transcribe.sh
+++ server/transcribe.sh
@@ -3,6 +3,6 @@ echo "THIS IS CURRENT WORKING"
 current_dir=$(pwd)
 cd whisper.cpp
 # ./whisper.cpp/main -f "$current_dir/data/processed/creationlabworkshop/creationlabworkshop.wav"
-./main -f "../$1"
+./main -f "../$1/$2" -otxt "../$1/$2.txt"
 cd ..
 # ./whisper.cpp/main -f samples/jfk.wav
