45053a78322066cda995a7aab3fa10618d21c957
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2024-09-13 22:38:32 -0700

    Convex script npm

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,7 +1,7 @@
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ConvexClientProvider } from "./ConvexClientProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
