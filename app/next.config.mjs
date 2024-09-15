5813a862b08120cb4a846c4a1f7f45bb83752d73
Priyanshu Mahey <72003945+priyanshumahey@users.noreply.github.com>
2024-09-15 00:28:21 -0700

    Routing search pre-write

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/localserver/:path*",
+        destination: "https://660e-129-97-124-74.ngrok-free.app/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
